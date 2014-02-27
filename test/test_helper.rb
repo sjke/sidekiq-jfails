d49a515578140bc501cc67531179c9d91b1719d2
sjke <biglolko@gmail.com>
2014-02-27 10:44:35 +0300

    Add codeclimate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,10 @@ require "rack/test"
 
 require "sidekiq"
 require "sidekiq-jfails"
+require "codeclimate-test-reporter"
 
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-Sidekiq.logger = nil
+CodeClimate::TestReporter.start
 REDIS = Sidekiq::RedisConnection.create(:url => "redis://localhost/15", :namespace => 'sidekiq_extension_test')
