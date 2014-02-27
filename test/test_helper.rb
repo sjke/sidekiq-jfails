ef2d13aae7c993af993a5cf461dbdefccc1f29d1
sjke <biglolko@gmail.com>
2014-02-27 11:47:20 +0300

    Change coverage system

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,10 +6,10 @@ require "rack/test"
 
 require "sidekiq"
 require "sidekiq-jfails"
-require "codeclimate-test-reporter"
+require 'coveralls'
+Coveralls.wear!
 
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-CodeClimate::TestReporter.start
 REDIS = Sidekiq::RedisConnection.create(:url => "redis://localhost/15", :namespace => 'sidekiq_extension_test')
